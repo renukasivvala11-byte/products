f5ad9c85ed229c21d1b686c3620c98b32f46987c
renukasivvala11-byte <renukasivvala11@gmail.com>
2026-05-27 10:08:00 +0530

    changes

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require("express");
-
 const cors = require("cors");
-
 const dotenv = require("dotenv");
 
 const connectDB = require("./config/db");
@@ -15,13 +13,14 @@ connectDB();
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
 
 app.use("/api/products", require("./routes/productRoutes"));
 
-const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.send("Welcome to Product API");
+});
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
 });
